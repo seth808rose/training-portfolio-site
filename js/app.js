673968a6f271bddc8248a48c10738e4a3635a095
seth808rose <69048387+seth808rose@users.noreply.github.com>
2021-08-22 08:58:35 -0700

    added simple fixed navbar when scroll

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const sidebar = document.querySelector('.sidebar')
-const closeBtn = document.querySelector('.close-btn')
+const toggleBtn = document.querySelector(".toggle-btn");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+const navbar = document.querySelector(".navbar-fixed");
 
-toggleBtn.addEventListener('click', function(){
-    sidebar.classList.toggle('show-sidebar')
-})
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
 
-closeBtn.addEventListener('click', function(){
-    sidebar.classList.remove('show-sidebar')
-})
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
 
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
