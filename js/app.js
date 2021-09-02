12be0ea2c75b34d8d410cf22965d0f4ffddb9299
seth808rose <69048387+seth808rose@users.noreply.github.com>
2021-09-01 21:09:49 -0700

    adding contact section (in progress)

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-const toggleBtn = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector(".sidebar");
 const navbar = document.querySelector(".nav");
-const closeBtn = document.querySelector(".close-btn");
+toggleBtn = document.querySelector(".toggle-btn");
+closeBtn = document.querySelector(".close-btn");
 
-toggleBtn.addEventListener("click", function () {
-  sidebar.classList.toggle("show-sidebar");
-});
-
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
-
+// fixed navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
   }
+});
+
+//toggle button
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+// close button
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
 });
