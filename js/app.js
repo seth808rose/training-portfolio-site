cb62ed86d1cca1c22003c1e6b10d5725cd2069bb
seth808rose <69048387+seth808rose@users.noreply.github.com>
2021-09-04 16:19:20 -0700

    fixed sizing for contact section in mobile view

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-const sidebar = document.querySelector(".sidebar");
 const navbar = document.querySelector(".nav");
-toggleBtn = document.querySelector(".toggle-btn");
-closeBtn = document.querySelector(".close-btn");
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
-// fixed navbar
+// fixed navabr
+
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
     navbar.classList.add("navbar-fixed");
@@ -12,11 +13,15 @@ window.addEventListener("scroll", function () {
   }
 });
 
-//toggle button
+// toggle button
+
 toggleBtn.addEventListener("click", function () {
   sidebar.classList.toggle("show-sidebar");
 });
-// close button
+
+// close sidebar button
+
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
